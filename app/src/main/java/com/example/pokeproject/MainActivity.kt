6fb4036fbe95d541e1e3d48df07bd1664c0300b7
Samuel Trujillo <Samuel.trujillo093@gmail.com>
2022-03-11 15:59:44 -0800

    Commit #2 Removed HomeActivity.kt and activity_home.xml added DashActivity.kt and activity.dash.xml
    add functionality to navigate to second activity via Sign in Button on MainActivity.kt
    
    fragment causing second activity to crash, commented out

--- app/src/main/java/com/example/pokeproject/MainActivity.kt
+++ app/src/main/java/com/example/pokeproject/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.pokeproject
 
-import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokeproject.view.DashActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,27 +11,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //set preferences, //builtInFunction("preferanceName", "PreferanceSharingSetting")
-        val userPreferences = getSharedPreferences("userPref", Context.MODE_PRIVATE)
-        //set edit function
-        val editPref = userPreferences.edit()
-
+        //Navigating to HomeActivity
+        //set signIn Button
+//        val toDash = findViewById<Button>(R.id.signIn)
+        //onListner for when signIn button is clicked
         signIn.setOnClickListener{
-            //Setting data input in EditTexts
-            val userName = userName.text.toString()
-            val favoritePoke = favoritePokeInput.text.toString()
-
-            //using assigned editor to apply shared preference data with Key,Value pairs
-            editPref.apply {
-                //defining Key, Value pairs
-                putString("userName", userName)
-                putString("favoritePoke", favoritePoke)
-                //saving/setting data input
-                apply()
-
-            }
-
+            //using an Intent to trigger onCreate method for HomeActivity
+            val dashIntent = Intent(this, DashActivity::class.java)
+            startActivity(dashIntent)
         }
+
+
+
+//        //set preferences, //builtInFunction("preferanceName", "PreferanceSharingSetting")
+//        val userPreferences = getSharedPreferences("userPref", Context.MODE_PRIVATE)
+//        //set edit function
+//        val editPref = userPreferences.edit()
+//
+//        signIn.setOnClickListener{
+//            //Setting data input in EditTexts
+//            val userName = userName.text.toString()
+//            val favoritePoke = favoritePokeInput.text.toString()
+//
+//            //using assigned editor to apply shared preference data with Key,Value pairs
+//            editPref.apply {
+//                //defining Key, Value pairs
+//                putString("userName", userName)
+//                putString("favoritePoke", favoritePoke)
+//                //saving/setting data input
+//                apply()
+//
+//            }
+//
+//        }
     }
 
 }
