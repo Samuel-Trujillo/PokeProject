8f0cd64ff83c37e01024b38fe305a147688e09c6
Samuel Trujillo <Samuel.trujillo093@gmail.com>
2022-03-12 14:24:48 -0800

    Commit #6
    started shared preference from scratch, removed from ActivityMain, created UserInfo class to hold shared preference data. Created UserSharedPreference to hold shared preference functionality

--- app/src/main/java/com/example/pokeproject/MainActivity.kt
+++ app/src/main/java/com/example/pokeproject/MainActivity.kt
@@ -12,33 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //prepping preference resources
-        //set preferences, //builtInFunction("preferanceName", "PreferanceSharingSetting")
-        val userPreferences = getSharedPreferences("userPref", Context.MODE_PRIVATE)
-        //set edit function
-        val editPref = userPreferences.edit()
-
         //Navigating to DashActivity
-        //Setting OnClickListener when signIn button is locked
+        //Setting OnClickListener when signIn button is clicked
         signIn.setOnClickListener{
             //using an Intent to trigger onCreate method for HomeActivity
             val dashIntent = Intent(this, DashActivity::class.java)
             startActivity(dashIntent)
-
-            //coding in shared preference functionality to signIN button
-            //Setting data input in EditTexts
-            val userName = userName.text.toString()
-            val favoritePoke = favoritePokeInput.text.toString()
-
-            //using assigned editor to apply shared preference data with Key,Value pairs
-            editPref.apply {
-                //defining Key, Value pairs
-                putString("userName", userName)
-                putString("favoritePoke", favoritePoke)
-                //saving/setting data input
-                apply()
-
-            }
         }
     }
 }
---
+++ app/src/main/java/com/example/pokeproject/model/UserInfo.kt
@@ -0,0 +1,6 @@
+package com.example.pokeproject.model
+
+data class UserInfo(
+    val uuName : String,
+    val uuPoke : String
+)
