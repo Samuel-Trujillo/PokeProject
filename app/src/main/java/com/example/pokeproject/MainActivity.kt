e6fa1ea48ff99fff5fa959b576432cc45705e26f
Samuel Trujillo <Samuel.trujillo093@gmail.com>
2022-03-12 14:52:48 -0800

    Commit #7
    shared preference not crashing app unless username and favorite pokemon fields are empty when navigating to second activity

--- app/src/main/java/com/example/pokeproject/MainActivity.kt
+++ app/src/main/java/com/example/pokeproject/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokeproject.model.UserInfo
+import com.example.pokeproject.model.UserSharedPreference
 import com.example.pokeproject.view.DashActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var uuinfo: UserSharedPreference
+    private lateinit var newUserInfo: UserInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +24,9 @@ class MainActivity : AppCompatActivity() {
             //using an Intent to trigger onCreate method for HomeActivity
             val dashIntent = Intent(this, DashActivity::class.java)
             startActivity(dashIntent)
+
+            uuinfo.updateUserInfo(newUserInfo)
+            println("hello")
         }
     }
 }
