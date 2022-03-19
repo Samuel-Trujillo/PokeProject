b419d4f2b372132b2a2c35f143fe7547920db651
Samuel Trujillo <Samuel.trujillo093@gmail.com>
2022-03-19 16:03:11 -0700

    Commit #23
    Set max value that could be input in the inputID ET so that it wont bring up a null query in DashActivity. Was able to separate/restructure functionality from DashActivity into ShowPokemonFragment in presenter package

---
+++ app/src/main/java/com/example/pokeproject/presenter/ShowPokemonFragment.kt
@@ -0,0 +1,49 @@
+package com.example.pokeproject.presenter
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.pokeproject.PokemonQuery
+import com.example.pokeproject.R
+import com.example.pokeproject.data.apolloClient
+import com.example.pokeproject.view.DashActivity
+import com.example.pokeproject.view.PokemonFragment
+import kotlinx.android.synthetic.main.activity_dash.*
+import kotlinx.android.synthetic.main.fragment_pokemon.*
+
+class ShowPokemonFragment {
+
+    fun showPokemonFragment(dashActivity: DashActivity){
+        //setting my fragment manager
+        val manager = dashActivity.supportFragmentManager
+        val transaction = manager.beginTransaction()
+        //setting fragment to replace placeholder
+        val fragment = PokemonFragment()
+        //the actual replacing
+        transaction.replace(R.id.fragmentHolder, fragment)
+
+        //setting query value
+        var searchByIdQuery = dashActivity.inputID.text.toString()
+        //casting inputID String to Integer
+        var pokeIdValue = Integer.parseInt(searchByIdQuery)
+        //passing through pokeIdValue into query
+        dashActivity.lifecycleScope.launchWhenCreated {
+            //setting ID search max as there are currently only 893 pokemon
+            if (pokeIdValue >= 894){
+                dashActivity.inputID.setError("There are only 893 Pokemon!")
+            }else{
+                var idResponse = apolloClient.query(PokemonQuery(pokeIdValue)).execute()
+                //setting query data to TextViews
+                dashActivity.onePokeTextView.text = idResponse.data?.pokemon?.name.toString()
+                dashActivity.onePokeTextViewGenus.text = idResponse.data?.pokemon?.genus.toString()
+                dashActivity.onePokeTextViewHeight.text = idResponse.data?.pokemon?.height.toString()
+                dashActivity.onePokeTextViewWeight.text = idResponse.data?.pokemon?.weight.toString()
+            }
+        }
+        //adding back button functionality to not close app
+        transaction.addToBackStack(null)
+        //saving/applying changes
+        transaction.commit()
+    }
+}
--- app/src/main/java/com/example/pokeproject/view/DashActivity.kt
+++ app/src/main/java/com/example/pokeproject/view/DashActivity.kt
@@ -1,26 +1,13 @@
 package com.example.pokeproject.view
-
-import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.core.content.ContentProviderCompat.requireContext
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.lifecycle.lifecycleScope
-import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.api.Query
-import com.example.pokeproject.PokemonQuery
 import com.example.pokeproject.R
 import kotlinx.android.synthetic.main.activity_dash.*
-import com.example.pokeproject.data.apolloClient
-import kotlinx.android.synthetic.main.fragment_pokemon.*
+
+import com.example.pokeproject.presenter.ShowPokemonFragment
 
 class DashActivity : AppCompatActivity() {
-
-    //creating my fragment manager
-    private val manager = supportFragmentManager
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash)
@@ -37,37 +24,13 @@ class DashActivity : AppCompatActivity() {
             text = uuPokeDisplay
         }
 
-        //replacing my fragment holder with actual data
-        fun showPokemonFragment(){
+        //instantiating ShowPokemonFragment
+        val initPokemonFragment = ShowPokemonFragment()
 
-            val transaction = manager.beginTransaction()
-            //setting fragment to replace placeholder
-            val fragment = PokemonFragment()
-            //the actual replacing
-            transaction.replace(R.id.fragmentHolder, fragment)
-
-            //setting query value
-            var searchByIdQuery = inputID.text.toString()
-            //casting inputID String to Integer
-            var pokeIdValue = Integer.parseInt(searchByIdQuery)
-            //passing through pokeIdValue into query
-            lifecycleScope.launchWhenCreated {
-                var idResponse = apolloClient.query(PokemonQuery(pokeIdValue)).execute()
-                //setting query data to TextViews
-                onePokeTextView.text = idResponse.data?.pokemon?.name.toString()
-                onePokeTextViewGenus.text = idResponse.data?.pokemon?.genus.toString()
-                onePokeTextViewHeight.text = idResponse.data?.pokemon?.height.toString()
-                onePokeTextViewWeight.text = idResponse.data?.pokemon?.weight.toString()
-            }
-            //adding back button functionality to not close app
-            transaction.addToBackStack(null)
-            //saving/applying changes
-            transaction.commit()
-        }
         //setting on click listener
         val showPokemon = findViewById<Button>(R.id.searchOne)
         showPokemon.setOnClickListener{
-            showPokemonFragment()
+            initPokemonFragment.showPokemonFragment(this)
         }
     }
 }
